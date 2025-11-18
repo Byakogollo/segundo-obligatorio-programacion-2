9ae09fcc68513b012ca44eed8b87d204ba1f31b2
EstebanNecuse <estebannecuse@gmail.com>
2025-11-18 20:37:01 -0300

    " cambio if linea 65 archivoCarga"

--- src/persistencia/ArchivoCargar.java
+++ src/persistencia/ArchivoCargar.java
@@ -17,81 +17,71 @@ import modelo.Sistema;
  * @author Byakogollo
  */
 public class ArchivoCargar {
-   private Scanner in;
-   private String linea;
-   
-   
-   
-       
-   public String cargarCurriculum(Empleado emp){
-       
-   String basePath = System.getProperty("user.dir") + "/cvs";
-    
-   String resultado = "";
-    
-      try{          
-      
-       this.in = new Scanner(Paths.get(basePath+"/CV"+emp.getCi()+".txt"));
-       
-       while(this.hayMasLineas()){
-           resultado += this.linea;
-       }
-        
-    }catch(IOException e){
-        
-          return e.toString();
-                
-                }
+    private Scanner in;
+    private String linea;
 
-    return resultado;
-}
-   
-   
-   private boolean hayMasLineas(){
-              
-       
-       if(this.in.hasNext()){
-           linea = in.nextLine();
-           return true;
-       }
-       
-       return false;
-       
-   }
-   
-   
-   
-   
-   
-   
-      public Sistema cargarSistema() throws Exception{
-          
-    String basePath = System.getProperty("user.dir") + "/persistencia";
-   
-    try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(basePath+"/Sistema"))) {
-        
-        Object obj = in.readObject();
-        
-        if (obj instanceof Sistema sistema) {
-            return sistema;
-            
-        } else {
-            
-           throw new Exception("El objeto leído no es una instancia de Sistema.");
-           
+    public String cargarCurriculum(Empleado emp) {
+
+        String basePath = System.getProperty("user.dir") + "/cvs";
+
+        String resultado = "";
+
+        try {
+
+            this.in = new Scanner(Paths.get(basePath + "/CV" + emp.getCi() + ".txt"));
+
+            while (this.hayMasLineas()) {
+                resultado += this.linea;
+            }
+
+        } catch (IOException e) {
+
+            return e.toString();
+
         }
-    } catch (IOException | ClassNotFoundException e) {
-        System.out.println("Error leyendo el archivo: " + e.getMessage());
+
+        return resultado;
     }
 
-    return null;
-}
-    
-    
-      
-      
-      
-      
-      
-    
+    private boolean hayMasLineas() {
+
+        if (this.in.hasNext()) {
+            linea = in.nextLine();
+            return true;
+        }
+
+        return false;
+
+    }
+
+    public Sistema cargarSistema() throws Exception {
+
+        String basePath = System.getProperty("user.dir") + "/persistencia";
+
+        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(basePath + "/Sistema"))) {
+
+            Object obj = in.readObject();
+
+            // if (obj instanceof Sistema sistema) {
+            // return sistema;
+
+            // }
+
+            if (obj instanceof Sistema) {
+                Sistema sistema = (Sistema) obj;
+                return sistema;
+            }
+
+            else {
+
+                throw new Exception("El objeto leído no es una instancia de Sistema.");
+
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println("Error leyendo el archivo: " + e.getMessage());
+        }
+
+        return null;
+    }
+
 }
